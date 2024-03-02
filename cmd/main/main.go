ccb4c911a1c1e4476f9868b33642411f17e7d284
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2024-03-02 16:35:58 -0300

    Configura uso da várivel de ambiente PORT e instala julienschmidt/httprouter

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,15 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"time"
+	"strconv"
+
+	env "github.com/Netflix/go-env"
+	"github.com/julienschmidt/httprouter"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
+type Environment struct {
+	PORT int `env:"PORT,default=3000"`
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":9999", nil)
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	var environment Environment
+	_, err := env.UnmarshalFromEnviron(&environment)
+	if err != nil {
+		return err
+	}
+
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	err = http.ListenAndServe(":"+strconv.Itoa(environment.PORT), router)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module rinha2024q1crebito
 
 go 1.21.4
+
+require (
+	github.com/Netflix/go-env v0.0.0-20220526054621-78278af1949d
+	github.com/julienschmidt/httprouter v1.3.0
+)
